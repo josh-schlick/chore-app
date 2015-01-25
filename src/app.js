50fd1535198d19321a8acc22e47e4fba26f244e1
Josh Schlick <joshdschlick@gmail.com>
2015-01-24 19:50:25 -0700

    now switching app.js to commonjs for more fun

--- src/app.js
+++ src/app.js
@@ -1,31 +1,22 @@
 /** @jsx React.DOM */
 
+var $ = require('jquery');
+var React = require('react');
+var TabbedArea = require('react-bootstrap/TabbedArea');
+var TabPane = require('react-bootstrap/TabPane');
+var UserTab = require('components/UserTab');
 
-define([
-	'jquery',
-	'react',
-	'react-bootstrap/TabbedArea',
-	'react-bootstrap/TabPane',
-	'components/UserTab'
-],
-function(
-	$,
-	React,
-	TabbedArea,
-	TabPane,
-	UserTab
-) {
-	React.initializeTouchEvents(true);
 
-	var mountNode = document.getElementById('content');
+React.initializeTouchEvents(true);
 
-		var tabbedAreaInstance = (
-		    <TabbedArea defaultActiveKey={1}>
-		      <TabPane eventKey={1} tab='Ashton'><UserTab /></TabPane>
-		      <TabPane eventKey={2} tab='Tonia'><UserTab /></TabPane>
-		      <TabPane eventKey={3} tab='Josh'><UserTab /></TabPane>
-		    </TabbedArea>
-		  );
+var mountNode = document.getElementById('content');
 
-	React.render(tabbedAreaInstance, mountNode);
-});
+	var tabbedAreaInstance = (
+		<TabbedArea defaultActiveKey={1}>
+		  <TabPane eventKey={1} tab='Ashton'><UserTab /></TabPane>
+		  <TabPane eventKey={2} tab='Tonia'><UserTab /></TabPane>
+		  <TabPane eventKey={3} tab='Josh'><UserTab /></TabPane>
+		</TabbedArea>
+	  );
+
+React.render(tabbedAreaInstance, mountNode);
