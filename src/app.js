8a530100254eac5720c2d69d85a7bc9623539a7b
Josh Schlick <joshdschlick@gmail.com>
2015-01-24 19:47:51 -0700

    switching app.js to amd for fun

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
 /** @jsx React.DOM */
 
-import $ from 'jquery';
-import React from'react';
-import TabbedArea from 'react-bootstrap/TabbedArea';
-import TabPane from 'react-bootstrap/TabPane';
-import UserTab from 'components/UserTab';
 
+define([
+	'jquery',
+	'react',
+	'react-bootstrap/TabbedArea',
+	'react-bootstrap/TabPane',
+	'components/UserTab'
+],
+function(
+	$,
+	React,
+	TabbedArea,
+	TabPane,
+	UserTab
+) {
+	React.initializeTouchEvents(true);
 
-React.initializeTouchEvents(true);
+	var mountNode = document.getElementById('content');
 
-var mountNode = document.getElementById('content');
+		var tabbedAreaInstance = (
+		    <TabbedArea defaultActiveKey={1}>
+		      <TabPane eventKey={1} tab='Ashton'><UserTab /></TabPane>
+		      <TabPane eventKey={2} tab='Tonia'><UserTab /></TabPane>
+		      <TabPane eventKey={3} tab='Josh'><UserTab /></TabPane>
+		    </TabbedArea>
+		  );
 
-	var tabbedAreaInstance = (
-	    <TabbedArea defaultActiveKey={1}>
-	      <TabPane eventKey={1} tab='Ashton'><UserTab /></TabPane>
-	      <TabPane eventKey={2} tab='Tonia'><UserTab /></TabPane>
-	      <TabPane eventKey={3} tab='Josh'><UserTab /></TabPane>
-	    </TabbedArea>
-	  );
-
-React.render(tabbedAreaInstance, mountNode);
+	React.render(tabbedAreaInstance, mountNode);
+});
